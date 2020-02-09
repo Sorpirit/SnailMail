e333b2bd32f2efb3094158f129662d03f6ba0874
ClitchLich <danvuk11@gmail.com>
2020-02-09 21:10:00 +0200

    More Particalse

---
+++ Assets/Scripts/ImpulsTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+public class ImpulsTest : MonoBehaviour
+{
+    
+    [SerializeField] CinemachineImpulseSource impulseSource;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V)){
+            impulseSource.GenerateImpulse();
+        }
+    }
+}
--- Assets/Scripts/MessageSpot.cs
+++ Assets/Scripts/MessageSpot.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class MessageSpot : MonoBehaviour
 {
     [SerializeField] private MessageTamplate message;
     [SerializeField] private ParticleSystem deliveredEffect;
+    [SerializeField] private ParticleSystem deliveredBlackHoleEffect;
     [SerializeField] private Color spotColor;
+    [SerializeField] private CinemachineImpulseSource impulseSource;
 
     private void Start() {
         deliveredEffect.startColor = spotColor;
+        deliveredBlackHoleEffect.startColor = spotColor;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -20,6 +24,8 @@ public class MessageSpot : MonoBehaviour
             controller.OnSpot();
             deliveredEffect.transform.position = other.transform.position;
             deliveredEffect.Play();
+            if(!deliveredBlackHoleEffect.isPlaying) deliveredBlackHoleEffect.Play();
+            impulseSource.GenerateImpulse();
         }
     }
 }
